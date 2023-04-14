1cd6e82059a0234f6bda648e6750bcce1d425d37
Bhushan0704 <bhushanschaudhari2023@gmail.com>
2023-04-14 18:44:42 +0530

    Data Types

--- src/Java/JavaBasics.java
+++ src/Java/JavaBasics.java
@@ -4,7 +4,7 @@ public class JavaBasics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Student s = new Student();
+		s.Details();
 	}
-
 }
---
+++ src/Java/Student.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Student {
+ public void Details() {
+	 	int StudentAge = 15;
+		double StudentCGPA =4.55;
+		char StudentFirstInitial = 'K';
+		boolean hasPerfectAttendance = true;
+		System.out.println("Student age is" +  " " + StudentAge + "\n" + "Student CGPA is" + " " + StudentCGPA + "\n" + "First Initial is" + " " + StudentFirstInitial +   "\n" + "Has Perfect Attendance"  + " " + hasPerfectAttendance);
+ }
+}
