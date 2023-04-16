e1cf2f540e2602f652fed97a49bd3f7a05615d43
Bhushan0704 <bhushanschaudhari2023@gmail.com>
2023-04-16 14:32:08 +0530

    Using JavaBuiltInFunctions

---
+++ src/Java/BuiltInJavaFunction.java
@@ -0,0 +1,11 @@
+package Java;
+//2*2*2*2*2 = 32.0
+//The Inbuild Math.pow function helps us to simplify
+public class BuiltInJavaFunction {
+
+	public static void main(String[] args) {
+		double result = Math.pow(2, 5);
+		System.out.println(result);
+
+	}
+}
