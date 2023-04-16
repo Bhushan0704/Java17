a3fff62c335325349d9113e0e8afbae24449171e
Bhushan0704 <bhushanschaudhari2023@gmail.com>
2023-04-16 13:59:34 +0530

    Function using parameters

--- src/Java/FunctionUsingParams.java
+++ src/Java/FunctionUsingParams.java
@@ -1,10 +1,15 @@
 package Java;
 
 public class FunctionUsingParams {
-
+	public static void calculateTotalMealPrice(double listedPrice, double taxRate, double tipRate) {
+	double tip = tipRate * listedPrice;
+	double tax = taxRate * listedPrice;
+	double result = listedPrice + tip + tax;
+	System.out.println("Your total meal price is :" + result);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		calculateTotalMealPrice(1, 10.80, 20.0);
 	}
 
 }
